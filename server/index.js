f519897424b5c2a605e5e0b0f9811f2d8dcfb9f4
Alex Yamaguchi <alexbilly2110@gmail.com>
2023-08-02 00:06:19 -0500

    successfully scrapes jobs & writes to csv

--- server/index.js
+++ server/index.js
@@ -53,6 +53,52 @@ const scrapeSite = async(site) => {
   }
 };
 
+// function for scraping jobs off site
+const scrapeJobs = async(site) => {
+  try {
+    // fetch HTML content
+    const jobQuery = await axios.get(site);
+
+    // load res data into Cheerio
+    const $ = cheerio.load(jobQuery.data);
+
+    // select job elems using CSS selector
+    const jobElems = $('.job-item');
+
+    // store scraped data into array
+    const jobs = [];
+
+    // iterate through jobElems —> extract data
+    jobElems.each((i, job) => {
+      const title = $(job)
+        .find('a').attr('title');
+
+      const company = $(job)
+        .find('.job-company').text().trim();
+
+      const place = $(job)
+        .find('.job-place').text().trim();
+
+      const type = $(job)
+        .find('.job-type').text().trim();
+
+      const date = $(job)
+        .find('.job-date').text().trim();
+
+      jobs.push({ title, company, place, type, date });
+    });
+
+    // output scraped jobs
+    console.log('Jobs successfully scraped!');
+    console.dir(jobs);
+
+    return jobs;
+
+  } catch(err) {
+    console.error(`Error scraping jobs: ${ err.message }`);
+  }
+};
+
 // function for writing data to CSV
 const writeToCsv = async(data) => {
   // create CSV writer w/ specified header fields
@@ -77,10 +123,39 @@ const writeToCsv = async(data) => {
   }
 };
 
+// function for writing jobs to CSV
+const writeJobsToCsv = async(data) => {
+  // create CSV writer w/ specified header fields
+  const csvWriter = createCsvWriter({
+    path: 'csv/jobs.csv',
+    header: [
+      { id: 'title', title: 'Author' },
+      { id: 'company', title: 'Company' },
+      { id: 'place', title: 'Place' },
+      { id: 'type', title: 'Type' },
+      { id: 'date', title: 'Date' }
+    ]
+  });
+
+  try {
+    await csvWriter.writeRecords(data);
+
+    // output message indicating success
+    console.log('Jobs successfully written to CSV file!');
+    // console.dir(data);
+
+  } catch(err) {
+    console.error(`Error writing data to CSV: ${ err }`);
+  }
+};
+
 // scrape site —> write data to CSV
 (async() => {
-  const quotes = await scrapeSite('http://quotes.toscrape.com/');
-  writeToCsv(quotes);
+  // const quotes = await scrapeSite('http://quotes.toscrape.com/');
+  // writeToCsv(quotes);
+
+  const jobs = await scrapeJobs('https://www.ranchwork.com/jcategory/seeking-ranch-job');
+  writeJobsToCsv(jobs);
 })();
 
 app.listen(PORT, () => {
